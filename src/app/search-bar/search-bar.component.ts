365c2b43f5828cfa7041345971134fac0fbce8db
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-11-21 13:52:00 +0100

    used search reducer

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,5 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormControl} from "@angular/forms";
+import {Store} from "@ngrx/store";
+import {setSearchTerm} from "../state/search.actions";
 
 @Component({
   selector: 'app-search-bar',
@@ -10,7 +12,11 @@ export class SearchBarComponent implements OnInit {
   @Output() searchTermEvent = new EventEmitter<string>();
   searchTermControl = new FormControl("");
 
+  constructor(
+    private store: Store
+  ) {}
+
   ngOnInit(): void {
-    this.searchTermControl.valueChanges.subscribe((searchTerm: string) => this.searchTermEvent.emit(searchTerm));
+    this.searchTermControl.valueChanges.subscribe((searchTerm: string) => this.store.dispatch(setSearchTerm({searchTerm})));
   }
 }
