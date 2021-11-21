a58a974ac56a32b483e60ea8a494403a4a6120b4
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-11-21 21:47:13 +0100

    modification

--- src/app/state/search.selector.ts
+++ src/app/state/search.selector.ts
@@ -1,8 +1,7 @@
 import {createFeatureSelector, createSelector} from "@ngrx/store";
 import {Search} from "../model/search.model";
-import {AppState} from "../model/state.model";
 
-const selectSearch = createFeatureSelector<AppState, Search>('search');
+const selectSearch = createFeatureSelector<Search>('search');
 
 export const selectSearchTerm = createSelector(
   selectSearch,
