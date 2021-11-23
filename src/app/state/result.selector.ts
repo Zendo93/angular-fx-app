c4ede92419061d5c0829b3b26d389b869ab82693
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-11-23 20:18:22 +0100

    modified result selector

--- src/app/state/result.selector.ts
+++ src/app/state/result.selector.ts
@@ -1,9 +1,20 @@
 import {createFeatureSelector, createSelector} from "@ngrx/store";
+import {IForeignExchange} from "../types";
+import {Search} from "../model/search.model";
 import {AppState} from "../model/state.model";
 
-const select = createFeatureSelector<AppState>('result');
+export const selectedSearchFeature = createFeatureSelector<Search>('search');
+export const selectedResultFeature = createFeatureSelector<AppState>('result');
 
 export const selectResult = createSelector(
-  select,
-  (state) => state.result
+  selectedSearchFeature,
+  selectedResultFeature,
+  (selectedSearchFeature: Search, selectedResultFeature: AppState) => {
+    let fx: IForeignExchange[] = [];
+    if(selectedResultFeature.result && selectedSearchFeature.term) {
+      fx = selectedResultFeature.result.fx.filter(fx => fx.currency.toLowerCase().startsWith(selectedSearchFeature.term.toLowerCase()));
+      return {...selectedResultFeature.result, fx: fx};
+    }
+    return selectedResultFeature?.result;
+  }
 );
