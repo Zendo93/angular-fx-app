d346cd7ee923e3fd62091ea489bb85ba15d66f0c
Richard Uzsak <richard.uzsak@siemens-healthineers.com>
2021-11-25 16:15:57 +0100

    cleaned app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-fx-app';
-  currentSearchTerm: string = "";
-
-  setSearchTerm(searchTerm: string) {
-    this.currentSearchTerm = searchTerm;
-  }
 }
